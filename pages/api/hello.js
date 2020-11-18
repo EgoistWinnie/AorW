3de9c6f4c7642fa01e82aecf31ca6551a6acc5e5
winnieinyourarea <thepiggywin2000@gmail.com>
2020-11-18 12:52:23 +0700

    init prisma

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+// export default (req, res) => {
+//   res.statusCode = 200
+//   res.json({ name: 'John Doe' })
+// }
+
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+async function main() {
+  // ... you will write your Prisma Client queries here
 }
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
